5b92b58ad5ffe3514456c4db9f144441b3b36dec
Tsotne Skhvitaridze <89193876+Daddygram@users.noreply.github.com>
2024-09-12 12:33:29 +0400

    project setup + navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import logo from '../assets/LOGO.png'
+
+const Navbar = () => {
+  return (
+    <div className="max-w-screen-2xl mx-auto py-[38px]">
+        <img src={logo} alt="redberry logo" />
+    </div>
+  )
+}
+
+export default Navbar

36ae707baea06ad893b441d00430d67e733babba
Tsotne Skhvitaridze <89193876+Daddygram@users.noreply.github.com>
2024-09-15 17:13:20 +0400

    simple react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import Navbar from './partials/Navbar.tsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './App.css'
+import Home from './pages/Home.tsx';
+import ListingPage from './pages/ListingPage.tsx';
+import AddListing from './pages/AddListing.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/listing",
+    element: <ListingPage />
+  },
+  {
+    path: "/add",
+    element: <AddListing />
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Navbar />
-    <App />
+    <div className='max-w-screen-2xl mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </StrictMode>,
 )
